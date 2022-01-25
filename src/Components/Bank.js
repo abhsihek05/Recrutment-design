6dd52cf84da32f7022e604045001a7acc0ce2faf
abhishek <jeet45171@gmail.com>
2022-01-25 17:23:54 +0530

    back commite

--- src/Components/Bank.js
+++ src/Components/Bank.js
@@ -13,7 +13,7 @@ export default class Bank extends React.Component {
                 <h1 className="text-center text-capitalize p-3 text-secondary">banking sector </h1>
                 <Carousel>
                     <Carousel.Item>
-                        <div className="parent-bank container d-flex justify-content-md-around">
+                        <div className="parent-bank    container d-flex justify-content-md-around">
                             <div className-="child-bank-div">
                                 <img src="./hdfc.png" alt="" />
                                 <span>hdfc bacnk</span>
